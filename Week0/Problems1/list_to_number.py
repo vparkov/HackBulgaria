97d8897a4dfe218e77bd0047814424a68e4b248b
Valentin <vparkov@gmail.com>
2014-10-16 20:12:23 +0300

    Deleting repeated files

--- Week0/Problems1/list_to_number.py
+++
@@ -1,6 +0,0 @@
-def list_to_number(digits):
-	num = int(''.join(map(str,digits)))
-	return num
-
-
-print(list_to_number([1,2,3]))
--- Week0/Problems1/signle_digit.py
+++
@@ -1,8 +0,0 @@
-def contains_digit(number, digit):
-	while number > 0:
-		if number % 10 == digit:
-			return True
-		number //= 10
-	return False
-
-print(contains_digit(434334, 5))
